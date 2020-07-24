273df8ea7a4bd6e63f519e15fc06c8e2c64f27af
Larrygf02 <raulgf_02@hotmail.com>
2020-07-24 00:02:10 -0500

    nodemon src index

--- node-mongo/src/database.js
+++ node-mongo/src/database.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 console.log('hola')
-mongoose.connect('mongodb://mongo/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+const DB_USER = "root"
+const DB_PASS = "123"
+const DB_HOST = "mongo"
+const DB_PORT = "27018"
+const DB_NAME = "mydatabase"
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`, { useNewUrlParser: true })
         .then(db => console.log('Db is conect', db.connection.host))
         .catch(err => console.log("Ocurrio un error con la conexion"))
--- node-mongo/src/index.js
+++ node-mongo/src/index.js
@@ -2,4 +2,5 @@ const express = require('express')
 
 const app = express()
 require('./database')
+app.use(require('./routes/index.routes'))
 app.listen(3000)
